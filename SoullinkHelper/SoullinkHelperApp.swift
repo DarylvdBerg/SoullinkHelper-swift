4929b7166e487c483a816760219d00f5bf9b4067
Darylvdberg <darylvdberg@gmail.com>
2022-12-10 17:53:27 +0100

    Initial sync

---
+++ SoullinkHelper/SoullinkHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoullinkHelperApp.swift
+//  SoullinkHelper
+//
+//  Created by Daryl van den Berg on 10/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct SoullinkHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
